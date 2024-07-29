10ccd928d983bb5b213a4a4764c60edec531a73a
Amin Karimi Monsefi <amin.karimi1992@gmail.com>
2024-07-29 22:11:34 +0000

    fix task 6

--- data_preparation/6-create_separate_semantic_with_arg.py
+++ data_preparation/6-create_separate_semantic_with_arg.py
@@ -33,7 +33,7 @@ def _process_panoptic_to_semantic(input_panoptic, output_semantic, segments, id_
         output[panoptic == seg["id"]] = new_cat_id
     Image.fromarray(output).save(output_semantic)
 
-def separate_coco_semantic_from_panoptic(panoptic_json, panoptic_root, sem_seg_root, categories):
+def separate_coco_semantic_from_panoptic(panoptic_json, panoptic_root, sem_seg_root, categories, add_log):
     os.makedirs(sem_seg_root, exist_ok=True)
 
     stuff_ids = [k["id"] for k in categories if k["isthing"] == 0]
@@ -59,21 +59,20 @@ def separate_coco_semantic_from_panoptic(panoptic_json, panoptic_root, sem_seg_r
             output = os.path.join(sem_seg_root, file_name)
             yield input, output, segments
 
-    print("Start writing to {} ...".format(sem_seg_root))
+    print_("Start writing to {} ...".format(sem_seg_root), add_log)
     start = time.time()
     pool.starmap(
         functools.partial(_process_panoptic_to_semantic, id_map=id_map),
         iter_annotations(),
         chunksize=100,
     )
-    print("Finished. time: {:.2f}s".format(time.time() - start))
+    print_("Finished. time: {:.2f}s".format(time.time() - start), add_log)
 
 def main():
     parser = argparse.ArgumentParser(description='Generate and save semantic segmentation masks from panoptic annotations.')
     parser.add_argument('--dataset_type', type=str, required=True, help='Type of dataset')
     parser.add_argument('--key_paths', type=str, nargs='+', required=True, help='List of key paths')
     parser.add_argument('--base_url', type=str, required=True, help='Base URL for the dataset')
-    parser.add_argument('--categories', type=str, required=True, help='Path to categories JSON file')
     parser.add_argument('--not_add_log', action="store_true", help='Add job log in the output or not')
 
     args = parser.parse_args()
@@ -83,7 +82,9 @@ def main():
     base_url = args.base_url
     add_log = not args.not_add_log
 
-    with open(args.categories, 'r') as f:
+    annotations_path = os.path.join(base_url, key_paths[0], '_annotations.coco.json')
+
+    with open(annotations_path, 'r') as f:
         info = json.load(f)
         
     categories = info['categories']
@@ -100,6 +101,7 @@ def main():
             os.path.join(base_url, "panoptic_masks/{}".format(key_path)),
             os.path.join(base_url, "panoptic_stuff/{}".format(key_path)),
             HIGHARC_CATEGORIES,
+            add_log
         )
 
 if __name__ == "__main__":
--- data_preparation/data_job.sh
+++ data_preparation/data_job.sh
@@ -8,7 +8,7 @@ BASE_URL="/home/ubuntu/dataset/brochure_construction"
 
 # rem Run job 1: Update COCO JSON file names
 echo Running job 1: Update COCO JSON file names
-echo ----------------------------------------------------------
+echo ---- 
 python 1-adding_isthing_to_file_with_arg.py --dataset_type $DATASET_TYPE --key_paths $KEY_PATHS --base_url $BASE_URL --not_add_log
 if [ $? -ne 0 ]; then
     echo "Job 1 failed!"
@@ -20,7 +20,7 @@ echo ---------------------------------------------------------------------------
 
 # rem Run job 2: Generate Panoptic based on COCO
 echo Running job 2: Generate Panoptic based on COCO
-echo ----------------------------------------------------------
+echo ---- 
 python 2-create_panoptic_annotations_file_with_arg.py --dataset_type $DATASET_TYPE --key_paths $KEY_PATHS --base_url $BASE_URL --not_add_log
 if [ $? -ne 0 ]; then
     echo "Job 2 failed!"
@@ -31,7 +31,7 @@ echo ---------------------------------------------------------------------------
 
 # rem Run job 3: Changes image names with png from jpg
 echo Running job 3: Changes image names with png from jpg
-echo ----------------------------------------------------------
+echo ---- 
 python 3-change_json_image_names_with_arg.py --dataset_type $DATASET_TYPE --key_paths $KEY_PATHS --base_url $BASE_URL --not_add_log
 if [ $? -ne 0 ]; then
     echo "Job 3 failed!"
@@ -42,7 +42,7 @@ echo ---------------------------------------------------------------------------
 
 # rem Run job 4: Generate png files from jpg
 echo Running job 4: Generate png files from jpg
-echo ----------------------------------------------------------
+echo ---- 
 python 4-convertor_jpg_to_png_with_arg.py --dataset_type $DATASET_TYPE --key_paths $KEY_PATHS --base_url $BASE_URL --not_add_log
 if [ $? -ne 0 ]; then
     echo "Job 4 failed!"
@@ -53,7 +53,7 @@ echo ---------------------------------------------------------------------------
 
 # rem Run job 5: Generate panoptic segmentation masks
 echo Running job 5: Generate panoptic segmentation masks
-echo ----------------------------------------------------------
+echo ---- 
 python 5-create_panoptic_masks_with_arg.py --dataset_type $DATASET_TYPE --key_paths $KEY_PATHS --base_url $BASE_URL --not_add_log
 if [ $? -ne 0 ]; then
     echo "Job 5 failed!"
@@ -64,8 +64,8 @@ echo ---------------------------------------------------------------------------
 
 # rem Run job 6: Generate semantic segmentation masks
 echo Running job 6: Generate semantic segmentation masks
-echo ----------------------------------------------------------
-python 6-create_separate_semantic_with_arg.py --dataset_type %DATASET_TYPE% --key_paths $KEY_PATHS --base_url $BASE_URL
+echo ---- 
+python 6-create_separate_semantic_with_arg.py --dataset_type $DATASET_TYPE --key_paths $KEY_PATHS --base_url $BASE_URL --not_add_log
 if [ $? -ne 0 ]; then
     echo "Job 6 failed!"
     exit 1
